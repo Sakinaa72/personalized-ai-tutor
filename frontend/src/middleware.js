5b04dbff601d0af172beb2cf761986b19880d800
Aman Kumar <kumar7080aman7080@gmail.com>
2025-07-22 00:47:41 +0530

    middleware comment

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -15,21 +15,24 @@ export default auth((req) => {
   const isLoggedIn = !!req.auth;
   const { nextUrl } = req;
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  console.log("url : ", nextUrl);
+  console.log("islogged : ", isLoggedIn);
 
-  if (isApiAuthRoute) {
-    return null;
-  } else if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return null;
-  } else if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL("/login", nextUrl));
-  }
-  return null;
+  // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  // const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  // if (isApiAuthRoute) {
+  //   return null;
+  // } else if (isAuthRoute) {
+  //   if (isLoggedIn) {
+  //     return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  //   }
+  //   return null;
+  // } else if (!isLoggedIn && !isPublicRoute) {
+  //   return Response.redirect(new URL("/login", nextUrl));
+  // }
+  // return null;
 });
 
 export const config = {
