ec5c2893aad80aab3c11cdfbb637a59618281876
Aman Kumar <kumar7080aman7080@gmail.com>
2025-07-21 23:28:36 +0530

    middleware and schema update

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -4,7 +4,18 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/login",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await prisma.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
-  ...authConfig,
+  ...authConfig
 });
--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -4,9 +4,10 @@ import authConfig from "./auth.config";
 const { auth } = NextAuth(authConfig);
 
 const publicRoutes = ["/"];
+
 const authRoutes = ["/login"];
 
-const apiAuthPrefix = "api/auth";
+const apiAuthPrefix = "/api/auth";
 
 const DEFAULT_LOGIN_REDIRECT = "/profile";
 
@@ -15,19 +16,17 @@ export default auth((req) => {
   const { nextUrl } = req;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
   if (isApiAuthRoute) {
     return null;
-  }
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  if (isAuthRoute) {
+  } else if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
     return null;
-  }
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-
-  if (!isLoggedIn && !isPublicRoute) {
+  } else if (!isLoggedIn && !isPublicRoute) {
     return Response.redirect(new URL("/login", nextUrl));
   }
   return null;
