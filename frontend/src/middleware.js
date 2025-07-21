7f9ce6341b13f990c8266d3e874623422df2208f
Aman Kumar <kumar7080aman7080@gmail.com>
2025-07-21 22:16:22 +0530

    middleware added for route checks

---
+++ frontend/src/middleware.js
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+const publicRoutes = ["/"];
+const authRoutes = ["/login"];
+
+const apiAuthPrefix = "api/auth";
+
+const DEFAULT_LOGIN_REDIRECT = "/profile";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  if (isApiAuthRoute) {
+    return null;
+  }
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/login", nextUrl));
+  }
+  return null;
+});
+
+export const config = {
+  // from https://clerk.com/docs/references/nextjs/clerk-middleware#usage
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
